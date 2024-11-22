4c2de908642040f14a73ad1d5a7f9ff282c86ce7
Patryk <patryko@bydnet.pl>
2024-11-22 16:58:13 +0100

    Typo fix

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
         pathname: "**",
       },
       {
-        protocolo: "https",
+        protocol: "https",
         hostname: "img.clerk.com",
         pathname: "**",
       },
